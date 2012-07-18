094d3710ffba40025d5e203a656830bc9cdc7a34
sl@minta.com <sl@minta.com>
2012-07-18 07:28:16 -0500

    balance;account summary

--- tests/WebService-Regression/simple.sh
+++ tests/WebService-Regression/simple.sh
@@ -2,6 +2,7 @@ CMD="curl -c cookie.txt -b cookie.txt -k -X POST https://pc-dev.com"
 
 # echo -e "\n\n*** Sign-up ***"
 # $CMD/json/user/account/signup/ -d '{ "email": "sl@pushcoin.com", "password": "test", "first_name": "Slaw", "last_name": "L.", "mobile_phone": "321-213-3123" }'
+
 echo -e "\n\n*** Login ***"
 $CMD/json/account/login/ -d '{ "user": "sl@pushcoin.com", "password": "test" }'
 
@@ -9,6 +10,12 @@ echo -e "\n\n*** Change Password ***"
 $CMD/json/account/change-password/ -d '{ "old_password": "test", "new_password": "test2" }'
 echo -e "\n\n*** Restore Password ***"
 $CMD/json/account/change-password/ -d '{ "old_password": "test2", "new_password": "test" }'
+
+echo -e "\n\n*** Balance ***"
+$CMD/json/account/balance/ -d ''
+
+echo -e "\n\n*** Account Summary ***"
+$CMD/json/account/summary/ -d ''
 
 echo -e "\n\n*** Logout ***"
 $CMD/json/account/logout/ -d ''
