f84f43a47a23ffda07ac9a5ef0db9bfaf1eb4b68
Slawomir Lisznianski <sl@pushcoin.com>
2013-06-28 00:30:17 -0500

    Bitsy initial project files

---
+++ Android/BitsyPOS/src/com/pushcoin/bitsypos/BitsyPosActivity.java
@@ -0,0 +1,15 @@
+package com.pushcoin.bitsypos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BitsyPosActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
