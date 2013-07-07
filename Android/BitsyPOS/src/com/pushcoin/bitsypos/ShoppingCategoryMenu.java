69f5f9a321e29a74fa99c8ab9f5ce6ff06ffd09f
Slawomir Lisznianski <sl@pushcoin.com>
2013-07-06 20:39:46 -0500

    Take data (model) out of the IconLabel view

--- Android/BitsyPOS/src/com/pushcoin/bitsypos/ShoppingCategoryMenu.java
+++ Android/BitsyPOS/src/com/pushcoin/bitsypos/ShoppingCategoryMenu.java
@@ -2,12 +2,8 @@ package com.pushcoin.bitsypos;
 
 import android.content.Context;
 import android.widget.ListView;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.view.View;
 import android.util.AttributeSet;
-import android.app.AlertDialog;
-import java.util.ArrayList;
 
 public class ShoppingCategoryMenu extends ListView
 {
@@ -17,44 +13,18 @@ public class ShoppingCategoryMenu extends ListView
 	public ShoppingCategoryMenu(Context context)
 	{
 		super(context);
-		init();
 	}
 
 	// If built from XML
 	public ShoppingCategoryMenu(Context context, AttributeSet attrs)
 	{
 		super(context, attrs);
-		init();
 	}
 	 
 	// If built from XML
 	public ShoppingCategoryMenu(Context context, AttributeSet attrs, int defStyle)
 	{
 		super(context, attrs, defStyle);
-		init();
-	}
-	 
-	public void init()
-	{
-		ArrayList<IconLabelArrayAdapter.Entry> menuItems = 
-			new ArrayList<IconLabelArrayAdapter.Entry>();
-
-		// populate menu item(s)
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.coffee_cup, "Breakfast") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.fries, "Lunch") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.pizza, "Hot Dinner") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.sushi_maki, "Salads") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.cheese_cake, "Deserts") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.wine_glass, "Beverages") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.toast, "Breads") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.apple, "Fruits") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.babelfish, "Fish") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.soft_drink, "Soda") );
-		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.cup_cake, "Snacks") );
-
-		menu_ = new IconLabelArrayAdapter(getContext(), R.layout.shopping_category_menu_row, R.id.shopping_category_menu_icon, R.id.shopping_category_menu_label, menuItems);
-		setAdapter(menu_);
-		setOnItemClickListener(new ListSelection());
 	}
 	 
 	@Override
@@ -78,18 +48,6 @@ public class ShoppingCategoryMenu extends ListView
 			}
 		}
 		return maxWidth;
-	}
-
-	private class ListSelection implements OnItemClickListener
-	{
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-		{
-			AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-			builder.setMessage("You pressed item #" + (position+1));
-			builder.setPositiveButton("OK", null);
-			builder.show();
-		}
 	}
 }
 
--- Android/BitsyPOS/src/com/pushcoin/bitsypos/ShoppingCategoryMenuFragment.java
+++ Android/BitsyPOS/src/com/pushcoin/bitsypos/ShoppingCategoryMenuFragment.java
@@ -1,16 +1,59 @@
 package com.pushcoin.bitsypos;
 
 import android.os.Bundle;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import java.util.ArrayList;
 
 public class ShoppingCategoryMenuFragment extends Fragment 
 {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
 	{
-		return inflater.inflate(R.layout.shopping_category_menu, container, false);
+		ListView view = (ListView)inflater.inflate(R.layout.shopping_category_menu, container, false);
+
+		ArrayList<IconLabelArrayAdapter.Entry> menuItems = 
+			new ArrayList<IconLabelArrayAdapter.Entry>();
+
+		// populate menu item(s)
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.coffee_cup, "Breakfast") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.fries, "Lunch") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.pizza, "Hot Dinner") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.sushi_maki, "Salads") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.cheese_cake, "Deserts") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.wine_glass, "Beverages") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.toast, "Breads") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.apple, "Fruits") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.babelfish, "Fish") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.soft_drink, "Soda") );
+		menuItems.add( new IconLabelArrayAdapter.Entry(R.drawable.cup_cake, "Snacks") );
+
+		model_ = new IconLabelArrayAdapter(getActivity(), R.layout.shopping_category_menu_row, R.id.shopping_category_menu_icon, R.id.shopping_category_menu_label, menuItems);
+		view.setAdapter(model_);
+
+		// install click-event listener
+		view.setOnItemClickListener(new ListSelection());
+
+		return view;
 	}
+
+	private class ListSelection implements OnItemClickListener
+	{
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+		{
+			AlertDialog.Builder builder = new AlertDialog.Builder( getActivity() );
+			builder.setMessage("You pressed item #" + (position+1));
+			builder.setPositiveButton("OK", null);
+			builder.show();
+		}
+	}
+
+	private IconLabelArrayAdapter model_;
 }
