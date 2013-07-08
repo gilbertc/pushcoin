5709a6d06fba33eeb07e6c5b727e4f27b2c94fd6
Slawomir Lisznianski <sl@pushcoin.com>
2013-07-08 00:28:18 -0500

    border around list-items

--- Android/BitsyPOS/src/com/pushcoin/bitsypos/ShoppingItemListFragment.java
+++ Android/BitsyPOS/src/com/pushcoin/bitsypos/ShoppingItemListFragment.java
@@ -20,15 +20,39 @@ public class ShoppingItemListFragment extends Fragment
 			new ArrayList<ItemSummaryArrayAdapter.Entry>();
 
 		// populate item(s)
-		items.add( new ItemSummaryArrayAdapter.Entry(R.drawable.coffee_cup, "Coffee", "Maine Roasted", "$3.75", R.drawable.cart_empty) );
-		items.add( new ItemSummaryArrayAdapter.Entry(R.drawable.coffee_cup, "Coffee", "Maine Roasted", "$3.75", R.drawable.cart_empty) );
-		items.add( new ItemSummaryArrayAdapter.Entry(R.drawable.coffee_cup, "Coffee", "Maine Roasted", "$3.75", R.drawable.cart_empty) );
+		for (int i = 0; i < 100; ++i)
+		{
+			items.add( new ItemSummaryArrayAdapter.Entry(icons_[i%icons_.length], "Coffee", "Maine Roasted", "$3.75", R.drawable.cart_empty_gray) );
+		}
 
 		model_ = new ItemSummaryArrayAdapter(getActivity(), R.layout.shopping_item_list_block, R.id.shopping_item_list_product, R.id.shopping_item_list_title, R.id.shopping_item_list_desc, R.id.shopping_item_list_price, R.id.shopping_item_list_indicator, items);
 		view.setAdapter(model_);
 
 		return view;
 	}
+
+	final int icons_[] = {
+		R.drawable.apple,
+		R.drawable.babelfish,
+		R.drawable.cake,
+		R.drawable.cake2,
+		R.drawable.cart_empty,
+		R.drawable.cart_empty_gray,
+		R.drawable.cheese_cake,
+		R.drawable.coffee_cup,
+		R.drawable.corbeille_box,
+		R.drawable.cup_cake,
+		R.drawable.fries,
+		R.drawable.fruit_cake,
+		R.drawable.ic_launcher,
+		R.drawable.pizza,
+		R.drawable.shopping_category_menu_row_selector,
+		R.drawable.shopping_item_list_selector,
+		R.drawable.soft_drink,
+		R.drawable.sushi_maki,
+		R.drawable.toast,
+		R.drawable.wine_glass, 
+	};
 
 	private ItemSummaryArrayAdapter model_;
 }
