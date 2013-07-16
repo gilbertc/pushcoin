cbb984ff3d6151e66c3dd3072651a7594bfd19fa
Slawomir Lisznianski <sl@pushcoin.com>
2013-07-15 23:13:30 -0500

    play with message handler to react to clicks

---
+++ Android/BitsyPos/src/com/pushcoin/bitsypos/IDispatcher.java
@@ -0,0 +1,8 @@
+package com.pushcoin.bitsypos;
+
+import android.os.Handler;
+
+public interface IDispatcher 
+{
+	Handler getDispachable();
+}
---
+++ Android/BitsyPos/src/com/pushcoin/bitsypos/MessageId.java
@@ -0,0 +1,6 @@
+package com.pushcoin.bitsypos;
+
+public class MessageId 
+{
+	final static int SHOPPING_CATEGORY_CLICKED = 1;
+}
