745b337ccfd985bd29f5f3542af9b52743bcc4e0
Slawomir Lisznianski <sl@pushcoin.com>
2013-08-25 11:14:25 -0500

    async-task prototyped

--- Android/IceBreaker/src/com/pushcoin/icebreaker/IceBreakerActivity.java
+++ Android/IceBreaker/src/com/pushcoin/icebreaker/IceBreakerActivity.java
@@ -1,15 +1,51 @@
 package com.pushcoin.icebreaker;
 
 import android.app.Activity;
+import android.widget.TextView;
 import android.os.Bundle;
+import android.os.AsyncTask;
 
 public class IceBreakerActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		TextView statusField = (TextView) findViewById(R.id.status_field);
+		new DownloadHistoryTask(statusField).execute("mat here");
+	}
+
+	private static class DownloadHistoryTask extends AsyncTask<String, Void, Long>
+	{
+		DownloadHistoryTask(TextView statusField)
+		{
+			statusField_ = statusField;
+		}
+
+		protected void onPreExecute()
+		{
+			statusField_.setText("Loading...");
+		}
+
+		protected Long doInBackground(String... mat)
+		{
+			// simulate long fetch
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {}
+
+			return 0L;
+		}
+
+		protected void onPostExecute(Long result) 
+		{
+			statusField_.setText("as of Today 5:15 PM");
+		}
+
+		TextView statusField_;
+	}
+
 }
