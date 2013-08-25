885a4f5711e030a6fe173b8be13c9554ed24215d
Slawomir Lisznianski <sl@pushcoin.com>
2013-08-25 14:12:36 -0500

    pre-PCOS

--- Android/IceBreaker/src/com/pushcoin/icebreaker/IceBreakerActivity.java
+++ Android/IceBreaker/src/com/pushcoin/icebreaker/IceBreakerActivity.java
@@ -2,8 +2,12 @@ package com.pushcoin.icebreaker;
 
 import android.app.Activity;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.os.Bundle;
 import android.os.AsyncTask;
+import com.pushcoin.pcos;
 
 public class IceBreakerActivity extends Activity
 {
@@ -14,24 +18,51 @@ public class IceBreakerActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		TextView statusField = (TextView) findViewById(R.id.status_field);
-		new DownloadHistoryTask(statusField).execute("mat here");
+		statusField_ = (TextView) findViewById(R.id.status_field);
+		refreshButton_ = (Button) findViewById(R.id.refresh_button);
+
+		refreshButton_.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				refreshView();
+			}
+		});
+
+		// fetch latest account state
+		refreshView();
+	}
+
+	private void refreshView()
+	{
+		// Load balance on startup
+		new DownloadHistoryTask(refreshButton_, statusField_).execute("mat here");
+	}
+
+  protected void onResume() 
+	{
+		super.onResume();
+		refreshView();
 	}
 
 	private static class DownloadHistoryTask extends AsyncTask<String, Void, Long>
 	{
-		DownloadHistoryTask(TextView statusField)
+		DownloadHistoryTask(Button refresh, TextView statusField)
 		{
+			refreshButton_ = refresh;
 			statusField_ = statusField;
 		}
 
 		protected void onPreExecute()
 		{
 			statusField_.setText("Loading...");
+
+			// stop useless repeatition
+			refreshButton_.setEnabled(false);
 		}
 
 		protected Long doInBackground(String... mat)
 		{
+			pcos.OutputBlock bo = new pcos.BlockWriter( "Bo" );
+
 			// simulate long fetch
 			try {
 				Thread.sleep(2000);
@@ -43,9 +74,15 @@ public class IceBreakerActivity extends Activity
 		protected void onPostExecute(Long result) 
 		{
 			statusField_.setText("as of Today 5:15 PM");
+
+			// allow manual refresh
+			refreshButton_.setEnabled(true);
 		}
 
+		Button refreshButton_;
 		TextView statusField_;
 	}
 
+	TextView statusField_;
+	Button refreshButton_;
 }
