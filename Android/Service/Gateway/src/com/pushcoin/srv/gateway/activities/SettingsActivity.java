0574e96f74bd239e60cca1d1b78d2ea0d75d120a
Slawomir Lisznianski <sl@pushcoin.com>
2013-11-09 18:45:50 -0600

    gateway: fix settings on large screens; deprecate old API

--- Android/Service/Gateway/src/com/pushcoin/srv/gateway/activities/SettingsActivity.java
+++ Android/Service/Gateway/src/com/pushcoin/srv/gateway/activities/SettingsActivity.java
@@ -28,34 +28,21 @@ import com.pushcoin.lib.core.net.Server;
 import com.pushcoin.lib.core.security.KeyStore;
 import com.pushcoin.lib.core.utils.Logger;
 
-public class SettingsActivity extends PreferenceActivity implements
-		OnSharedPreferenceChangeListener {
-
+public class SettingsActivity 
+	extends PreferenceActivity 
+	implements OnSharedPreferenceChangeListener
+{
 	private static Logger log = Logger.getLogger(SettingsActivity.class);
-	private static final boolean ALWAYS_SIMPLE_PREFS = false;
 
-	@SuppressWarnings("deprecation")
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-
-		getPreferenceManager().setSharedPreferencesName(Preferences.NAME);
-		getPreferenceManager().setSharedPreferencesMode(MODE_PRIVATE);
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		getPreferenceManager().getSharedPreferences()
-				.registerOnSharedPreferenceChangeListener(this);
-
-		setupSimplePreferencesScreen();
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		super.onCreateOptionsMenu(menu);
 		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
@@ -75,30 +62,31 @@ public class SettingsActivity extends PreferenceActivity implements
 		}
 	}
 
-	private AlertDialog showResetDeviceDialog() {
+	private AlertDialog showResetDeviceDialog()
+	{
 		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-		dialog.setTitle("Reset Device");
-		dialog.setMessage("Your device will be unlinked with your account. Are you sure?");
-		dialog.setPositiveButton("Yes, reset",
+		dialog.setTitle(getString(R.string.dialog_reset_device_title));
+		dialog.setMessage(getString(R.string.dialog_reset_device_prompt));
+		dialog.setPositiveButton(getString(R.string.dialog_reset_device_yes),
 				new DialogInterface.OnClickListener() {
 					@Override
-					public void onClick(DialogInterface dialogInterface, int i) {
-						KeyStore keyStore = KeyStore
-								.getInstance(SettingsActivity.this);
-						if (keyStore != null)
+					public void onClick(DialogInterface dialogInterface, int i)
+					{
+						KeyStore keyStore = KeyStore.getInstance(SettingsActivity.this);
+						if (keyStore != null) {
 							keyStore.reset(SettingsActivity.this);
+						}
 
-						Toast.makeText(SettingsActivity.this,
-								"Device reset completed", Toast.LENGTH_LONG)
-								.show();
+						Toast.makeText(SettingsActivity.this, 
+							getString(R.string.dialog_reset_device_toast), 
+							Toast.LENGTH_LONG).show();
 
-						Intent myIntent = new Intent(SettingsActivity.this,
-								BootstrapActivity.class);
+						Intent myIntent = new Intent(SettingsActivity.this, BootstrapActivity.class);
 						startActivity(myIntent);
 						SettingsActivity.this.finish();
 					}
 				});
-		dialog.setNegativeButton("No, go back",
+		dialog.setNegativeButton(getString(R.string.dialog_reset_device_no),
 				new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialogInterface, int i) {
@@ -107,80 +95,50 @@ public class SettingsActivity extends PreferenceActivity implements
 		return dialog.show();
 	}
 
-	@SuppressWarnings("deprecation")
-	private void setupSimplePreferencesScreen() {
-		if (!isSimplePreferences(this)) {
-			return;
-		}
-		addPreferencesFromResource(R.xml.pref_general);
-
-		bindPreferenceSummaryToValue(getPreferenceManager(),
-				findPreference(Preferences.PREF_URL));
-	}
-
 	@Override
-	public boolean onIsMultiPane() {
-		return isXLargeTablet(this) && !isSimplePreferences(this);
+	public void onBuildHeaders(List<Header> target)
+	{
+		loadHeadersFromResource(R.xml.pref_headers, target);
 	}
 
-	private static boolean isXLargeTablet(Context context) {
-		return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-	}
+	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = 
+		new Preference.OnPreferenceChangeListener()
+			{
+				@Override
+				public boolean onPreferenceChange(Preference preference, Object value)
+				{
+					String stringValue = value.toString();
 
-	private static boolean isSimplePreferences(Context context) {
-		return ALWAYS_SIMPLE_PREFS
-				|| Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
-				|| !isXLargeTablet(context);
-	}
+					if (preference instanceof ListPreference)
+					{
+						ListPreference listPreference = (ListPreference) preference;
+						int index = listPreference.findIndexOfValue(stringValue);
 
-	@Override
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public void onBuildHeaders(List<Header> target) {
-		if (!isSimplePreferences(this)) {
-			loadHeadersFromResource(R.xml.pref_headers, target);
-		}
-	}
+						preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
 
-	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-		@Override
-		public boolean onPreferenceChange(Preference preference, Object value) {
-			String stringValue = value.toString();
+					} else {
+						preference.setSummary(stringValue);
+					}
+					return true;
+				}
+			};
 
-			if (preference instanceof ListPreference) {
-				ListPreference listPreference = (ListPreference) preference;
-				int index = listPreference.findIndexOfValue(stringValue);
-
-				preference
-						.setSummary(index >= 0 ? listPreference.getEntries()[index]
-								: null);
-
-			} else {
-				preference.setSummary(stringValue);
-			}
-			return true;
-		}
-	};
-
-	private static void bindPreferenceSummaryToValue(
-			PreferenceManager preferenceManager, Preference preference) {
+	private static void bindPreferenceSummaryToValue(PreferenceManager preferenceManager, Preference preference)
+	{
 		// Set the listener to watch for value changes.
-		preference
-				.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+		preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
-		String value = preferenceManager.getSharedPreferences().getString(
-				preference.getKey(), "");
-
+		String value = preferenceManager.getSharedPreferences().getString(preference.getKey(), "");
 		log.i("binding " + preference.getKey() + " value: " + value);
-
-		sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-				value);
-
+		sBindPreferenceSummaryToValueListener.onPreferenceChange(preference, value);
 	}
 
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class GeneralPreferenceFragment extends PreferenceFragment {
+	public static class GeneralPreferenceFragment extends PreferenceFragment
+	{
 		@Override
-		public void onCreate(Bundle savedInstanceState) {
+		public void onCreate(Bundle savedInstanceState)
+		{
 			super.onCreate(savedInstanceState);
 			getPreferenceManager().setSharedPreferencesName(Preferences.NAME);
 			getPreferenceManager().setSharedPreferencesMode(MODE_PRIVATE);
@@ -191,16 +149,17 @@ public class SettingsActivity extends PreferenceActivity implements
 	}
 
 	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
-		
-		if (key.equals(Preferences.PREF_URL)) {
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+	{
+		if (key.equals(Preferences.PREF_URL))
+		{
 			String value = sharedPreferences.getString(key, "");
 			log.d("preference changed: " + key + " check: " + value);
 
 			Server.setDefaultUrl(value);
 			KeyStore keyStore = KeyStore.getInstance(this);
-			if (keyStore.hasMAT()) {
+			if (keyStore.hasMAT())
+			{
 				TransactionKeyService.scheduleAlarms(this, 0, false);
 				Toast.makeText(this, "Transaction Key Service Active",
 						Toast.LENGTH_LONG).show();
