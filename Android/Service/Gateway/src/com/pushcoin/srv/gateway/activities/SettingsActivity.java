84d4644dcd1fe65e59cf02a50dc1faa4bbe69db8
Slawomir Lisznianski <sl@pushcoin.com>
2013-11-09 21:47:02 -0600

    fix: configuration changes aren't recognized right-away

--- Android/Service/Gateway/src/com/pushcoin/srv/gateway/activities/SettingsActivity.java
+++ Android/Service/Gateway/src/com/pushcoin/srv/gateway/activities/SettingsActivity.java
@@ -1,20 +1,16 @@
 package com.pushcoin.srv.gateway.activities;
 
-import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceFragment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -28,9 +24,7 @@ import com.pushcoin.lib.core.net.Server;
 import com.pushcoin.lib.core.security.KeyStore;
 import com.pushcoin.lib.core.utils.Logger;
 
-public class SettingsActivity 
-	extends PreferenceActivity 
-	implements OnSharedPreferenceChangeListener
+public class SettingsActivity extends PreferenceActivity 
 {
 	private static Logger log = Logger.getLogger(SettingsActivity.class);
 
@@ -86,12 +80,14 @@ public class SettingsActivity
 						SettingsActivity.this.finish();
 					}
 				});
+
 		dialog.setNegativeButton(getString(R.string.dialog_reset_device_no),
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialogInterface, int i) {
-					}
-				});
+				new DialogInterface.OnClickListener()
+					{
+						@Override
+						public void onClick(DialogInterface dialogInterface, int i) {
+						}
+					});
 		return dialog.show();
 	}
 
@@ -101,40 +97,9 @@ public class SettingsActivity
 		loadHeadersFromResource(R.xml.pref_headers, target);
 	}
 
-	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = 
-		new Preference.OnPreferenceChangeListener()
-			{
-				@Override
-				public boolean onPreferenceChange(Preference preference, Object value)
-				{
-					String stringValue = value.toString();
-
-					if (preference instanceof ListPreference)
-					{
-						ListPreference listPreference = (ListPreference) preference;
-						int index = listPreference.findIndexOfValue(stringValue);
-
-						preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
-
-					} else {
-						preference.setSummary(stringValue);
-					}
-					return true;
-				}
-			};
-
-	private static void bindPreferenceSummaryToValue(PreferenceManager preferenceManager, Preference preference)
-	{
-		// Set the listener to watch for value changes.
-		preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-		String value = preferenceManager.getSharedPreferences().getString(preference.getKey(), "");
-		log.i("binding " + preference.getKey() + " value: " + value);
-		sBindPreferenceSummaryToValueListener.onPreferenceChange(preference, value);
-	}
-
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-	public static class GeneralPreferenceFragment extends PreferenceFragment
+	public static class GeneralPreferenceFragment
+		extends PreferenceFragment
+		implements SharedPreferences.OnSharedPreferenceChangeListener
 	{
 		@Override
 		public void onCreate(Bundle savedInstanceState)
@@ -143,27 +108,76 @@ public class SettingsActivity
 			getPreferenceManager().setSharedPreferencesName(Preferences.NAME);
 			getPreferenceManager().setSharedPreferencesMode(MODE_PRIVATE);
 			addPreferencesFromResource(R.xml.pref_general);
-			bindPreferenceSummaryToValue(getPreferenceManager(),
-					findPreference(Preferences.PREF_URL));
+			bindPreferenceSummaryToValue(getPreferenceManager(), findPreference(Preferences.PREF_URL));
 		}
-	}
 
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
-	{
-		if (key.equals(Preferences.PREF_URL))
+		@Override
+		public void onResume()
 		{
-			String value = sharedPreferences.getString(key, "");
-			log.d("preference changed: " + key + " check: " + value);
+			super.onResume();
+			getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+		}
 
-			Server.setDefaultUrl(value);
-			KeyStore keyStore = KeyStore.getInstance(this);
-			if (keyStore.hasMAT())
+		@Override
+		public void onPause()
+		{
+			getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+			super.onPause();
+		}
+
+		@Override
+		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+		{
+			if (key.equals(Preferences.PREF_URL))
 			{
-				TransactionKeyService.scheduleAlarms(this, 0, false);
-				Toast.makeText(this, "Transaction Key Service Active",
-						Toast.LENGTH_LONG).show();
+				Context context = getActivity();
+				String value = sharedPreferences.getString(key, "");
+				log.d("preference changed: " + key + " check: " + value);
+
+				Server.setDefaultUrl(value);
+				KeyStore keyStore = KeyStore.getInstance(context);
+				if (keyStore.hasMAT())
+				{
+					TransactionKeyService.scheduleAlarms(context, 0, false);
+					Toast.makeText(context, getString(R.string.settings_have_mat), Toast.LENGTH_LONG).show();
+				}
 			}
 		}
+
+		/**
+			Shows summary of the preference.
+		*/
+		private static void bindPreferenceSummaryToValue(PreferenceManager preferenceManager, Preference preference)
+		{
+			// Set the listener to watch for value changes.                                                                 
+			preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+			String value = preferenceManager.getSharedPreferences().getString( preference.getKey(), "");
+			log.i("binding " + preference.getKey() + " value: " + value);
+			// Show what's currently configured
+			sBindPreferenceSummaryToValueListener.onPreferenceChange( preference, value );
+		}
+
+		private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = 
+			new Preference.OnPreferenceChangeListener()
+				{
+					@Override
+					public boolean onPreferenceChange(Preference preference, Object value)
+					{
+						String stringValue = value.toString();
+
+						if (preference instanceof ListPreference)
+						{
+							ListPreference listPreference = (ListPreference) preference;
+							int index = listPreference.findIndexOfValue(stringValue);
+
+							preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);                                                                                            
+						} else {
+							preference.setSummary(stringValue);
+						}
+						return true;
+					}
+  };
+
 	}
 }
